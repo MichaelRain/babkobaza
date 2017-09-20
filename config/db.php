a3932fdd1b222709ec7ac52eb1047b9452ba0bad
Michael Rain <michaelrain@imonlyone.ru>
2017-09-21 00:54:20 +0500

    You rly dont need to know, sry :*

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=kasyanovma_yii',
-    'username' => 'kasyanovma_yii',
-    'password' => 'Dbrnjhbz11',
-    'charset' => 'utf8',
-];
